f654a02ec8612ba58d6def796287d7a8b0e018bb
Md. Shaiful Islam <islambuet@gmail.com>
2020-10-27 22:08:13 +0600

    changed error vues

--- src/components/not_found/NotFound.vue
+++ src/components/not_found/NotFound.vue
@@ -1,10 +1,9 @@
 <template>
-    <div>
-        <h1>{{$system_functions.get_label('msg_404_title')}}</h1>
-        <h3>{{$system_functions.get_label('msg_404_header')}}</h3>
-        <p>{{$system_functions.get_label('msg_404_message')}}</p>   
-        <a-button onclick="window.location.reload();" type="primary">{{$system_functions.get_label('button_refresh_page')}}</a-button>        
-    </div>
+  <a-result status="404" :title="$system_functions.get_label('msg_404_title')" :sub-title="$system_functions.get_label('msg_404_message')">
+    <template #extra>
+      <a-button onclick="window.location.reload();" type="primary">{{$system_functions.get_label('button_refresh_page')}}</a-button>  
+    </template>
+  </a-result>
 </template>
 <script>
 export default { 
--- src/views/busy_states/LoadingFailed.vue
+++ src/views/busy_states/LoadingFailed.vue
@@ -1,8 +1,7 @@
 <template>
-    <div>
-        <h1>{{$system_functions.get_label('msg_loading_failed_title')}}</h1>
-        <h3>{{$system_functions.get_label('msg_loading_failed_header')}}</h3>
-        <p>{{$system_functions.get_label('msg_loading_failed_message')}}</p>   
-        <a-button onclick="window.location.reload();" type="primary">{{$system_functions.get_label('button_refresh_page')}}</a-button>        
-    </div>
+  <a-result status="500" :title="$system_functions.get_label('msg_loading_failed_title')" :sub-title="$system_functions.get_label('msg_loading_failed_message')">
+    <template #extra>
+      <a-button onclick="window.location.reload();" type="primary">{{$system_functions.get_label('button_refresh_page')}}</a-button>  
+    </template>
+  </a-result>
 </template>
